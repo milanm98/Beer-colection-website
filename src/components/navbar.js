e3ba544ea25d4ed9fc840fbd688c2381aa61953f
milanm98 <marjanovmilan13@gmail.com>
2021-04-22 13:50:09 -0400

    background

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,10 +1,10 @@
-function navbar(){
+function navbar(){  
     return (
         <main>
             <div className="w-full h-auto text-center">
-                <a href="https://www.facebook.com/dragana.jerkov" rel="noreferrer" target="_blank" className="float-left mx-4 md:mx-10 my-5 fa fa-facebook-square"><p className="hidden">a</p></a>
+                <a href="https://www.facebook.com/zeljko.cumura" rel="noreferrer" target="_blank" className="float-left mx-4 md:mx-10 my-5 fa fa-facebook-square"><p className="hidden">a</p></a>
                 <a href="https://www.facebook.com/dragana.jerkov" rel="noreferrer" target="_blank" className="mx-4 md:mx-10 my-5 fa fa-search"><p className="hidden">a</p></a>
-                <a href="https://www.facebook.com/dragana.jerkov" rel="noreferrer" target="_blank" className="float-right mx-4 md:mx-10 my-5 fa fa-instagram"><p className="hidden">a</p></a>
+                <a href="https://www.instagram.com/cumurazeljko/" rel="noreferrer" target="_blank" className="float-right mx-4 md:mx-10 my-5 fa fa-instagram"><p className="hidden">a</p></a>
             </div>
         </main>
     )
