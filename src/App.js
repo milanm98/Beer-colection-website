9bd4a5784abfc7eed1d82404216adc83af0209e5
milanm98 <marjanovmilan13@gmail.com>
2021-04-22 12:55:41 -0400

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import { Helmet } from 'react-helmet'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Helmet>
+        <title>Zeljko Cumura</title>
+        <meta name="description" content="Kolekcija piva" />
+        <meta name="theme-color" content="#008f68" />
+      </Helmet>      
+
+
     </div>
   );
 }
