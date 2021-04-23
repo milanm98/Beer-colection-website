b496c1768a79be9f5f60f61f814d2fa7b2eb2225
milanm98 <marjanovmilan13@gmail.com>
2021-04-23 12:50:11 -0400

    latest project feature

---
+++ src/components/addNewBeers.js
@@ -0,0 +1,9 @@
+function addNewBeers(){
+    return(
+        <main>
+
+        </main>
+    )
+}
+
+export default addNewBeers;
--- src/components/beers.js
+++ src/components/beers.js
@@ -15,8 +15,14 @@ function Beers(){
     const [input,setInput] = useState("");
     const [searchType, setSearchType] = useState("");
 
-    const updateSelect = ( selected ) => {
-        setSearchType(selected.target.value);
+    const setCountry = () => {
+        setSearchType("country");
+        alert("Pretraga po drzavama");
+    }
+
+     const setName = () => {
+        setSearchType("name");
+        alert("Pretraga po imenima");
     }
 
     const update = (markdown) => {
@@ -37,11 +43,17 @@ function Beers(){
 
     return(
         <main>
+            <div className="text-center p-6">
+                <div className="flex gap-24 justify-center">
+                    <div className="">
+                        <label onClick={setCountry} className="fa fa-globe text-white"></label>
+                    </div>
+                    <div className="">
+                        <label onClick={setName} className="fa fa-pencil-square-o text-white"></label>
+                    </div>
+                </div>
+            </div>
             <div className="text-center text-white text-2xl font-black">
-                <select onChange={updateSelect} className="mt-6 w-32 text-white bg-black border-2 border-white">
-                    <option value="country">Drzava</option>
-                    <option value="name">Ime</option>
-                </select>
                 <input onChange={update} value={input} className="border-2 border-white mt-20 w-5/6 h-12 text-2xl text-center text-white bg-gray-900" type="text"></input>
             </div>
             <div className="w-full text-center h-auto bg-gray-900">
